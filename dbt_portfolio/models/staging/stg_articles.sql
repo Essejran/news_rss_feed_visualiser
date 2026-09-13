34755f759809d7ce38634b5a071bffd723d2cc61
Lay <lnyrabia@gmail.com>
2026-09-13 16:49:44 +0200

    changed source to Global Voices in order to make repository public, since Global Voices use a Creative Commons license which allows for public use with proper referencing

--- dbt_portfolio/models/staging/stg_articles.sql
+++ dbt_portfolio/models/staging/stg_articles.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='view') }}
 
 with source as (
-    select * from {{ source('tso_feed', 'articles_raw') }}
+    select * from {{ source('global_voices', 'articles_raw') }}
 ),
 
 deduped as (
@@ -17,13 +17,21 @@ select
     link,
     author,
     pub_date,
-    initcap(replace(
-        regexp_extract(link, r'https?://[^/]+/([^/]+)/'),
+    -- Extracts the article slug from URL structure (e.g. globalvoices.org/YYYY/MM/DD/slug/)
+    replace(
+        regexp_extract(link, r'https?://[^/]+/\d{4}/\d{2}/\d{2}/([^/]+)/?'),
         '-', ' '
-    )) as main_category,
-    categories as tags,
+    ) as article_slug,
+    -- Cleans category tags array by stripping trailing/leading whitespace
+    array(
+        select trim(cat)
+        from unnest(categories) as cat
+        where cat is not null and trim(cat) != ''
+    ) as tags,
     description,
     content_html,
-    pulled_at as last_seen_at
+    pulled_at as last_seen_at,
+    -- Standardized CC-BY 3.0 attribution for downstream charts & dashboards
+    'Original content from Global Voices (https://globalvoices.org), published under CC BY 3.0.' as source_attribution
 from deduped
 where rn = 1
